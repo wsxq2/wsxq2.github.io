35c3cb988e1a6c90dc7ef96da9db961a4265335f
wsxq2 <wsxq2@qq.com>
2019-12-01 13:10:15 +0800

    update

--- js/my.js
+++ js/my.js
@@ -1,3 +1,12 @@
+function isPC(){
+    var userAgentInfo = navigator.userAgent;
+    var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) { flag = false; break; }
+    }
+    return flag;
+}
 $(function() {
     $(".backtop-button").hide();
     $(function() {
@@ -15,41 +24,47 @@ $(function() {
             return false;
         });
     });
-	let toc=document.querySelector("#markdown-toc+ul");
-	let sc=document.getElementById('catalog-content');
-	/*判断是否生成了目录(有的文章内容短无目录)*/
-	if(toc) 
-		sc.innerHTML= toc.innerHTML;
-	else
-		sc.innerHTML= '本文没有目录 -_-';
-	$('.catalog-button').click(function() {
-		let sc=document.getElementById('sidebar-catalog');
-		if (sc.style.left==="-300px") {
-			$('#sidebar-catalog').animate({left:"+=300px"},300);
-			$('article').animate({left:'+=150px'},300);
-			$('header').animate({left:'+=150px'},300);
-		} else {
-			$('#sidebar-catalog').animate({left:"-=300px"},300);
-			$('article').animate({left:'-=150px'},300);
-			$('header').animate({left:'-=150px'},300);
-		}
-	});
 
-	// Get the modal
-	var modal = document.getElementById('myModal');
+    if(isPC()){
+        $('#sidebar-catalog').css("left","0px");
+    } else{
+        $('#sidebar-catalog').css("left","-300px");
+    }
+    let toc=document.querySelector("#markdown-toc+ul");
+    let sc=document.getElementById('catalog-content');
+    /*判断是否生成了目录(有的文章内容短无目录)*/
+    if(toc)
+        sc.innerHTML= toc.innerHTML;
+    else
+        sc.innerHTML= '本文没有目录 -_-';
+    $('.catalog-button').click(function() {
+        let sc=document.getElementById('sidebar-catalog');
+        if (sc.style.left==="-300px") {
+            $('#sidebar-catalog').animate({left:"+=300px"},300);
+            $('article').animate({left:'+=150px'},300);
+            $('header').animate({left:'+=150px'},300);
+        } else {
+            $('#sidebar-catalog').animate({left:"-=300px"},300);
+            $('article').animate({left:'-=150px'},300);
+            $('header').animate({left:'-=150px'},300);
+        }
+    });
 
-	// Get the image and insert it inside the modal - use its "alt" text as a caption
-	var imgs = document.getElementsByTagName('img')
-	var modalImg = document.getElementById("img01");
-	var captionText = document.getElementById("caption");
-	for (let i = 0, len = imgs.length; i < len; i++) {
-		imgs[i].onclick = function(){
-			modal.style.display = "block";
-			modalImg.src = this.src;
-			captionText.innerHTML = this.alt;
-		}
-	}
-	modalImg.onclick=function() {
-		modal.style.display = "none";
-	}
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var imgs = document.getElementsByTagName('img')
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    for (let i = 0, len = imgs.length; i < len; i++) {
+        imgs[i].onclick = function(){
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+    }
+    modalImg.onclick=function() {
+        modal.style.display = "none";
+    }
 });
